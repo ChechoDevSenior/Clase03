ea163cac2e15a1eb6dbedb858bf0a58e1560f4aa
Sergio Arturo Pe?a Chaparro <sergioarturo9306@gmail.com>
2024-11-11 14:10:43 -0500

    Ejercicio2

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ejercicio2 {
     public static void main(String[] args) {
                 /*
@@ -5,6 +7,14 @@ public class Ejercicio2 {
          en porción de 100 gramos cuesta 5,95€ y el vendedor nos dice que el precio por kilo cuesta 29,75€. 
          ¿Crees que es correcto el precio?
          */
-        
+        var scanner = new Scanner (System.in);
+        System.out.print("Ingrese el nombre del producto: ");
+        var producto = scanner.nextLine();
+        var porcion100Gr = 5.95d;
+        var precioKg = porcion100Gr*10d;
+        System.out.println("El precio del kilo de "+producto+"es: "+precioKg+"€.");
     }
+    /*
+     RTA/ El valor del precio 29,75€ es incorrecto porque el valor real del precio del kilo de pernil es 59.5€
+     */
 }
