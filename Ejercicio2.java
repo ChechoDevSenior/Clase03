4cd913226862efd0e3d2d6f99effc91b215ab89c
Sergio Arturo Pe?a Chaparro <sergioarturo9306@gmail.com>
2024-11-11 13:50:21 -0500

    Ejercicio2

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,5 +1,10 @@
 public class Ejercicio2 {
     public static void main(String[] args) {
+                /*
+         En el mercado, podemos encontrar que el precio del producto **Pernil Iberic D'Engreix Llen. Azuaga**
+         en porción de 100 gramos cuesta 5,95€ y el vendedor nos dice que el precio por kilo cuesta 29,75€. 
+         ¿Crees que es correcto el precio?
+         */
         
     }
 }
